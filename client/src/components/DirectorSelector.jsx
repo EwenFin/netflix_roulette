331892b3a0790631592f41657bca3276c2aacb2f
Ewen Carr <ewencarr@hotmail.com>
2017-05-02 15:38:01 +0100

    done

--- client/src/components/DirectorSelector.jsx
+++ client/src/components/DirectorSelector.jsx
@@ -1,13 +1,32 @@
-// import React from 'react';
+import React from 'react';
 
-// class DirectorSelector extends React.Component {
+class DirectorSelector extends React.Component {
 
-  
+constructor(props){
+    super(props);
+    // this.handleChange = this.handleChange.bind(this);
+    // this.setIndex = this.setIndex.bind(this)
+    this.state = {
+      selectedIndex: 0
+    }
+  }
 
-  
-// }
+  handleChange(event){
+    const directorIndex = event.target.value
+    console.log(directorIndex)
+  }
+
+  render() {
+    return (
+      <select id="director" onChange = {this.handleChange.bind(this)}>
+        <option value={0} key= {0}> Steven Spielberg </option>
+        <option value={1} key= {1}> Quentin Tarantino </option>
+      </select>
+    );
+  }
+}
 
 
 
 
-// export deafult DirectorSelector;
+export default DirectorSelector;
--- client/src/containers/MovieContainer.jsx
+++ client/src/containers/MovieContainer.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import MovieSelector from '../components/MovieSelector';
 import MovieDetail from '../components/MovieDetail';
+import DirectorSelector from '../components/DirectorSelector';
 
 class MovieContainer extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       movies: [],
-      selectedMovie: null
+      selectedMovie: null,
+      directorIndex: null
     };
   }
 
@@ -15,8 +17,33 @@ class MovieContainer extends React.Component {
     this.setState({ selectedMovie: movie })
   }
 
+  updateDirector(index) {
+      const directorsAddresses = ["Quentin%20Tarantino", "Steven%20Spielberg"]
+      const director = directorsAddresses[index]
+      const url = "http://netflixroulette.net/api/api.php?director=" + director
+
+      const request = new XMLHttpRequest();
+      request.open("GET", url);
+
+      request.onload = () => {
+        if( request.status === 200){
+          
+          const jsonString = request.responseText;
+          const data = JSON.parse(jsonString);
+          console.log(data)
+          this.setState({ movies:data, selectedMovie: data[0]})
+        }
+      }
+
+      request.send();
+    }
+
+  
+
   componentDidMount(){
-    const url = "http://netflixroulette.net/api/api.php?director=Quentin%20Tarantino"
+    
+    const url = "http://netflixroulette.net/api/api.php?director=Quentin%20Tarantino" 
+
     const request = new XMLHttpRequest();
     request.open("GET", url);
 
@@ -39,6 +66,8 @@ class MovieContainer extends React.Component {
         <h2>Tarantino Movie Container</h2>
         <MovieSelector movies = {this.state.movies} setSelectedMovie = {this.setSelectedMovie.bind(this)}/>
         <MovieDetail movie = {this.state.selectedMovie} />
+        <h2>Director Selector Container</h2>
+        <DirectorSelector setIndex={this.updateDirector.bind(this)}/>
       </div>
     );
   }
